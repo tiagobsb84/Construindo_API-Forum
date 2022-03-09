1dd9d26121f01987da0efd5a99ae508118151592
Tiago Silva <tiagobsb84@gmail.com>
2022-03-09 09:34:16 -0300

    Criando projeto

---
+++ src/main/java/br/com/tiago/forum/ConstruindoApiForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiago.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstruindoApiForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstruindoApiForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiago/forum/ConstruindoApiForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiago.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruindoApiForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
