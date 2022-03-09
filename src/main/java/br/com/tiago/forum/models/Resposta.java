2372ab9c9bbdfc24f71e11114f0910ef0f2bf97f
Tiago Silva <tiagobsb84@gmail.com>
2022-03-09 20:30:37 -0300

    Adicionando Cardinalidade

--- src/main/java/br/com/tiago/forum/models/Resposta.java
+++ src/main/java/br/com/tiago/forum/models/Resposta.java
@@ -3,12 +3,22 @@ package br.com.tiago.forum.models;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Resposta {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 	
--- src/main/java/br/com/tiago/forum/models/Usuario.java
+++ src/main/java/br/com/tiago/forum/models/Usuario.java
@@ -2,8 +2,15 @@ package br.com.tiago.forum.models;
 
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
