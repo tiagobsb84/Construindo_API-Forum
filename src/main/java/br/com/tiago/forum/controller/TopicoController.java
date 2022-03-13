ba253d95efa26771147e393bbf83fb3521af816e
Tiago Silva <tiagobsb84@gmail.com>
2022-03-12 21:08:46 -0300

    Fazendo validacoes no arquivo TopicoForm

--- src/main/java/br/com/tiago/forum/controller/TopicoController.java
+++ src/main/java/br/com/tiago/forum/controller/TopicoController.java
@@ -3,6 +3,8 @@ package br.com.tiago.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +42,7 @@ public class TopicoController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
--- src/main/java/br/com/tiago/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/tiago/forum/controller/form/TopicoForm.java
@@ -1,13 +1,23 @@
 package br.com.tiago.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.tiago.forum.models.Curso;
 import br.com.tiago.forum.models.Topico;
 import br.com.tiago.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull @NotEmpty @Length(min = 2)
 	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 3)
 	private String mensagem;
+	
+	@NotNull @NotEmpty
 	private String nomeCurso;
 	
 	public String getTitulo() {
