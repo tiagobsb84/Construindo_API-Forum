05dd246c93e5de4fb004c0398990bd299501b228
Tiago Silva <tiagobsb84@gmail.com>
2022-03-19 16:35:23 -0300

    Trocando de Lista para paginacao o Metodo GET

--- src/main/java/br/com/tiago/forum/dtos/TopicoDTO.java
+++ src/main/java/br/com/tiago/forum/dtos/TopicoDTO.java
@@ -1,8 +1,8 @@
 package br.com.tiago.forum.dtos;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
 
 import br.com.tiago.forum.models.Topico;
 
@@ -33,8 +33,8 @@ public class TopicoDTO {
 		return dataCriacao;
 	}
 
-	public static List<TopicoDTO> converter(List<Topico> topico) {
-		return topico.stream().map(TopicoDTO :: new).collect(Collectors.toList());
+	public static Page<TopicoDTO> converter(Page<Topico> topico) {
+		return topico.map(TopicoDTO::new);
 	}
 	
 	
--- src/main/java/br/com/tiago/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/tiago/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.tiago.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.tiago.forum.models.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCurso_Nome(String nomeCurso);
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 	
 }
