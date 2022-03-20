f7902e05d2f656dd9860bbaf7dd890b8aea160b3
Tiago Silva <tiagobsb84@gmail.com>
2022-03-20 16:47:43 -0300

    Adicionando parametro padrao

--- src/main/java/br/com/tiago/forum/controller/TopicoController.java
+++ src/main/java/br/com/tiago/forum/controller/TopicoController.java
@@ -8,9 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,9 +43,7 @@ public class TopicoController {
 
 	@GetMapping
 	public Page<TopicoDTO> listaTopico(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int quantidade, @RequestParam String ordenacao){
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.DESC, ordenacao);
+			@PageableDefault(sort = "id", direction = Direction.DESC) Pageable paginacao){
 		
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
