5ab634d7cccbbaced8a6817954612100ce49f49d
Tiago Silva <tiagobsb84@gmail.com>
2022-03-24 15:11:51 -0300

    Adicionando configuracao do cache

--- src/main/java/br/com/tiago/forum/ConstruindoApiForumApplication.java
+++ src/main/java/br/com/tiago/forum/ConstruindoApiForumApplication.java
@@ -2,14 +2,15 @@ package br.com.tiago.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class ConstruindoApiForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConstruindoApiForumApplication.class, args);
 	}
-
 }
