94e4ab1b5b7c251b9372d144cb6afade073ee0a6
Tiago Silva <tiagobsb84@gmail.com>
2022-03-25 17:14:51 -0300

    Liberando a saida do EndPoint dos metodos GET com a configuracao de seguranca

--- src/main/java/br/com/tiago/forum/config/securaty/SecurityConfiguration.java
+++ src/main/java/br/com/tiago/forum/config/securaty/SecurityConfiguration.java
@@ -1,5 +1,34 @@
 package br.com.tiago.forum.config.securaty;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		super.configure(auth);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		super.configure(http);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		super.configure(web);
+	}
 }
